ff2f8305f604c6d767fdf6751c68257c17cf784a
d2525456d <72094644+d2525456d@users.noreply.github.com>
2021-04-20 09:04:50 +0700

    Add files via upload

---
+++ bubble_sort.R
@@ -0,0 +1,14 @@
+bubble_sort <- function(x){
+  n<-length(x)
+  for(j in 1:(n-1)){
+    for(i in 1:(n-j)){
+      if(x[i]>x[i+1]){
+        temp<-x[i]
+        x[i]<-x[i+1]
+        x[i+1]<-temp
+      }
+    }
+  }
+  return(x)
+}
+
