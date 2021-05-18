c8afa72551d79b32cd1ef8686e49da8410adb482
d2525456d <72094644+d2525456d@users.noreply.github.com>
2021-05-18 08:21:02 +0700

    Add files via upload

---
+++ 18052021chuog5.R
@@ -0,0 +1,22 @@
+setwd('D:\\downloads\\New folder')
+#chuong 5 => plot=>bieu do dang x,y
+#them mot so tool cho ve bieu do
+#phan tich du lei
+#7.1.Pie Chart
+#H5N1 Avian infuenza A=>who world health organization
+BFCases = read.table('BirdFluCases.txt',header=TRUE)
+names(BFCases)
+str(BFCases)
+#thao tac thong ke so case theo nam
+Cases =rowSums(BFCases[,2:16])
+names(Cases)=BFCases[,1]
+Cases
+#ve bieu do pie chart
+#bieu do chuan - standard pie chart
+par(mfrow=c(2,2),mar=c(3,3,2,1))
+pie(Cases,main="Stadard pie chart")
+pie(Cases,main="Grey color", col=gray(seq(0.4,1.0,length=6)),clockwise=TRUE)
+pie(Cases,main="rainbow color", col=rainbow(6),clockwise=TRUE)
+install.packages("plotrix")
+library(plotrix)
+pie3D(Cases,main="3d pie chart", explode=0.1,labels=names(Cases),labelces=0.6,clockwise=TRUE)
